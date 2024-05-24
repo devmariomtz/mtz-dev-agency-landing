cf45e4280940cbb52b99441e3684bf8f942ddba3
mariomtzdev <99843020+devmariomtz@users.noreply.github.com>
2024-05-23 22:26:17 -0600

    FIX: Improve the Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,12 @@ import { TypeAnimation } from "react-type-animation";
 const Hero = () => {
   return (
     <div className="w-full h-full flex flex-col  lg:p-20 xl:p-20">
-      <p className="animate-fade-down text-[2em] font-black text-center p-4 lg:text-4xl xl:text-5xl">
+      <span className="animate-fade-down text-[2em] font-black text-center p-4 lg:text-4xl xl:text-5xl">
         CONSTRUYAMOS ALGO EXTRAORDINARIO
-      </p>
+      </span>
       <div className="flex flex-col xl:flex-row lg:flex-row">
         <div className="w-full flex flex-col items-center xl:p-5 lg:p-5 xl:items-end lg:items-end lg:gap-4">
+
           <TypeAnimation
             sequence={[
               // Same substring at the start will only be typed once, initially
@@ -21,14 +22,14 @@ const Hero = () => {
               1500,
             ]}
             speed={40}
-            className="text-center text-[1.5em] p-2 lg:text-4xl lg:text-end font-bold w-full text-blue-700"
+            className="text-center text-[1.7em] p-2 lg:text-4xl lg:text-end font-bold w-full text-blue-700"
             repeat={Infinity}
           />
           <a
             href="/"
-            className="animate-jump-in w-[50%] text-center xl:text-2xl lg:text-2xl bg-blue-700 hover:bg-blue-500 cursor-pointer text-white font-bold py-2 px-4 border-b-4 border-blue-900 hover:border-blue-700 rounded"
+            className="flex items-center justify-center animate-jump-in w-[50%] text-center xl:text-2xl lg:text-2xl bg-blue-700 hover:bg-blue-500 cursor-pointer text-white font-bold py-2 px-4 border-b-4 border-blue-900 hover:border-blue-700 rounded"
           >
-            🚀 CUENTANOS TU IDEA
+            <span className="text-2xl md:text-3xl lg:text-3xl block animate-wiggle-more animate-infinite">🚀</span> <span className="pl-2">CUENTANOS TU IDEA</span>
           </a>
         </div>
         <img
